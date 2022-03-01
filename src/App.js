dfbfd79cd6359ab5a991893eba3ba78818898718
sebafrugar <sebafrugar@gmail.com>
2022-03-01 17:48:19 -0400

    practicando props en react js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+import PersonCard from './components/apoyalo'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PersonCard  firstName = "Jane" lastName="doe" age="45" hairColor="black"/> 
+      <PersonCard  firstName = "John" lastName="Smith" age="88" hairColor="brown"/>
+      <PersonCard  firstName = "Millard" lastName="Fillmore" age="50" hairColor="brown"/>
+      <PersonCard  firstName = "Maria" lastName="Smith" age="62" hairColor="brown"/>
     </div>
   );
 }
