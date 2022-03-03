085a22f044361b84691103cc1d0e9ffefdcca245
sebafrugar <sebafrugar@gmail.com>
2022-03-03 16:18:07 -0400

    creacion de boton de aumento y disminucion de edad

---
+++ src/components/apoyalo.jsx
@@ -0,0 +1,34 @@
+import React, { Component} from 'react';
+
+class PersonCard extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+          contador : 0
+        }; 
+    }
+    botonIncrementar(){
+        this.setState({
+            contador: this.state.contador+1
+        });
+    }
+    botonMeEquivoque(){
+        this.setState({
+            contador: this.state.contador-1
+        })
+    }
+    render(){
+        const {firstName,lastName,age,hairColor} = this.props;
+        return(
+            <div>
+                <h1>{lastName} , {firstName} </h1>
+                <p>Age: {parseInt(age)+ this.state.contador},  </p>
+                <p>Hair Color: {hairColor}</p>
+                <button onClick={ ()=> this.botonIncrementar()}>Boton de cumpleaños de {firstName} {lastName}</button><br />
+                <button onClick={()=> this.botonMeEquivoque()}>Me equivoque en la edad </button>
+            </div>
+        )
+    }
+}
+
+export default PersonCard;
